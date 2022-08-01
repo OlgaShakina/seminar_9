5f3661abbbfb16805d1b8f1fce20c4f34bce964c
Olga <olga.buinova@gmail.com>
2022-08-02 00:14:33 +0300

    новые задания

---
+++ exercise_64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+int sum(int m, int n){
+    if(m+1==n){
+        return m+n;
+    }
+    else {
+        return  sum(m,n-1)+n;
+        }
+}
+
+Console.WriteLine("Введите два числа промежутка M и N");//m<n
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+if(n<=m){Console.WriteLine("Введены некорректные значения");}
+else{Console.WriteLine("Сумма числе в промежутке MN равна " +sum(m,n));}
